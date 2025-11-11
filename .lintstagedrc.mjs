983f74cdc4da52f8e84f289e8290cdac17ebf025
mufarodev <mufaro.priv@gmail.com>
2025-11-11 17:18:22 +0100

    feat: Husky setup and lint-staged

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,12 @@
+export default {
+  "Cartesian.Frontend/src/**/*.{ts,svelte}": [
+    "pnpm --filter cartesian.frontend run lint --fix",
+  ],
+  "Cartesian.Frontend/**/*.{json,md}": [
+    "pnpm --filter cartesian.frontend run format",
+  ],
+
+  "**/*.cs": ["dotnet format --verify-no-changes --include"],
+
+  "*.json": ["prettier --write"],
+};
