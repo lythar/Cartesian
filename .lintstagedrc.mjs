897c5062affa52edd1644d1e1de60cb689930c22
mufarodev <mufaro.priv@gmail.com>
2025-11-11 17:22:34 +0100

    fix: husky setup without package duplication

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,9 +1,22 @@
+const stripAbsolutePath = (filenames) => {
+  return filenames.map((f) => {
+    const match = f.match(/Cartesian\.Frontend\/(.+)/);
+    return match ? match[1] : f;
+  });
+};
+
 export default {
-	"Cartesian.Frontend/src/**/*.{ts,svelte}": [
-		"pnpm --filter cartesian.frontend run lint:fix",
-	],
-	"Cartesian.Frontend/**/*.{json,md}": [
-		"pnpm --filter cartesian.frontend run format",
-	],
-	"**/*.cs": ["dotnet format --verify-no-changes --include"],
+  "Cartesian.Frontend/src/**/*.{ts,svelte}": (filenames) => {
+    const files = stripAbsolutePath(filenames).join(" ");
+    return [
+      `sh -c "cd Cartesian.Frontend && pnpm exec prettier --write ${files}"`,
+    ];
+  },
+  "Cartesian.Frontend/**/*.{json,md}": (filenames) => {
+    const files = stripAbsolutePath(filenames).join(" ");
+    return [
+      `sh -c "cd Cartesian.Frontend && pnpm exec prettier --write ${files}"`,
+    ];
+  },
+  "**/*.cs": ["dotnet format --verify-no-changes --include"],
 };
--- Cartesian.Frontend/src/routes/map/+page.svelte
+++ Cartesian.Frontend/src/routes/map/+page.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
-    import mapboxgl from 'mapbox-gl';
-	import { onMount } from 'svelte';
+	import mapboxgl from "mapbox-gl";
+	import { onMount } from "svelte";
 
-    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
-    
-    let map: mapboxgl.Map;
+	mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
-    onMount(() => {
-        map = new mapboxgl.Map({
-            container: 'lythar-map',
-            style: 'mapbox://styles/mufarodev/cmhty7aqj001h01s9cqw2ao4g',
-            center: [-74.5, 40], // starting position [lng, lat]
-            zoom: 9 // starting zoom,
-        });
-    })
+	let map: mapboxgl.Map;
+
+	onMount(() => {
+		map = new mapboxgl.Map({
+			container: "lythar-map",
+			style: "mapbox://styles/mufarodev/cmhty7aqj001h01s9cqw2ao4g",
+			center: [-74.5, 40], // starting position [lng, lat]
+			zoom: 9, // starting zoom,
+		});
+	});
 </script>
 
-
-<div id="lythar-map" class="w-screen h-screen">
-</div>
+<div id="lythar-map" class="test-class h-screen w-screen"></div>
