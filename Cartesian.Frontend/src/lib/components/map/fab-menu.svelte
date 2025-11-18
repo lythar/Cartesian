b7db2c4d24e2e015c2ed7e38156f456430375431
mufarodev <mufaro.priv@gmail.com>
2025-11-18 20:39:57 +0100

    feat: give fab-menu button functionality

--- Cartesian.Frontend/src/lib/components/map/fab-menu.svelte
+++ Cartesian.Frontend/src/lib/components/map/fab-menu.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { Button } from "$lib/components/ui/button";
+	import { cn } from "$lib/utils";
 	import { PlusSignIcon } from "@hugeicons/core-free-icons";
 	import { HugeiconsIcon } from "@hugeicons/svelte";
+	import { newEventOverlayState } from "./map-state.svelte";
 
 </script>
 
@@ -9,7 +11,11 @@
 	<Button
 		variant="default"
     size="lg"
-		class="shadow-2xl transition-all duration-200"
+    onclick={() => {
+      newEventOverlayState.open = true;
+      newEventOverlayState.source = "create";
+    }}
+		class={cn("shadow-2xl transition-all duration-200", newEventOverlayState.open ? "scale-0 opacity-0 pointer-events-none" : "scale-100 opacity-100")}
 	>
     <HugeiconsIcon icon={PlusSignIcon} size={24} strokeWidth={2} />
     <span class="font-bold">Create an Event</span>
