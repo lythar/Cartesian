7145b9a4242a9e32ca04b8dc0adbcb85522be33b
Mufaro <81554673+mufarodev@users.noreply.github.com>
2025-11-18 12:17:48 +0000

    feat: update new event overlay design

--- Cartesian.Frontend/src/lib/components/map/new-event-overlay.svelte
+++ Cartesian.Frontend/src/lib/components/map/new-event-overlay.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import type { LatLng } from "$lib/utils";
+	import { cn, type LatLng } from "$lib/utils";
 	import { animate } from "motion";
 	import { newEventOverlayState } from "./map-state.svelte";
 	import Button from "../ui/button/button.svelte";
+	import Label from "../ui/label/label.svelte";
 
     interface Props {
         map: mapboxgl.Map;
@@ -16,53 +17,64 @@
     let draftLocation: LatLng | null = $state<LatLng | null>(null);
 
     $effect(() => {
-       console.log("hi");
         open = newEventOverlayState.open;
-        // if (open) {
-        //     draftLocation = newEventOverlayState.location;
-        //     if (overlayContainer)
-        //       animate(overlayContainer,
-        //         { scale: [0.85, 1], opacity: [0, 1], filter: ["blur(6px)", "blur(0px)"] },
-        //         { duration: 0.35, ease: [0.32, 0.72, 0, 1] }
-        //       );
-        // } else {
-        //     if (overlayContainer) animate(overlayContainer, { scale: [1, 0.30], opacity: 0, x: 50 }, { duration: 0.2 }).then(() => {
-        //         draftLocation = null;
-        //     });
-        // }
+        if (open) {
+            if (overlayContainer) {
+                animate(
+                    overlayContainer,
+                    {
+                        opacity: [0, 1],
+                        transform: ["scale(0.90)", "scale(1)"]
+                    },
+                    {
+                        duration: 0.25,
+                        ease: [.45, -.05, .15, 1.05]
+                    }
+                );
+            }
+        } else {
+            if (overlayContainer) {
+                animate(
+                    overlayContainer,
+                    {
+                        opacity: [1, 0],
+                        transform: ["scale(1)", "scale(0.90)"]
+                    },
+                    {
+                        duration: 0.25,
+                        ease: [.45, -.05, .15, 1.05]
+                    }
+                );
+            }
+        }
     });
 </script>
 
 <div
-  class:open={open}
-  bind:this={overlayContainer} class="overlay-container absolute top-0 right-4 pb-4 pt-20 z-50 origin-bottom-right w-2/7 h-full">
-    {#if open}
-        <div class="h-full w-full rounded-2xl bg-background shadow-lg p-6 flex flex-col">
-            <h2 class="text-2xl font-semibold mb-4">New Event</h2>
-            <Button
-              class="self-end mb-4"
-              onclick={() => {
-                  newEventOverlayState.open = false;
-              }}
-            >Close</Button>
-      </div>
-    {/if}
+  bind:this={overlayContainer} class={cn("absolute top-0 right-4 z-50 pt-20 pb-4 origin-right w-140 h-full flex flex-col pointer-events-none", open ? "" : "")}>
+    <div class={cn("flex-1 flex flex-col", open ? "pointer-events-auto" : "pointer-events-none")}>
+        <div class="rounded-lg bg-background shadow-lg flex flex-col justify-between h-full">
+            <div class="flex-1 p-6">
+                <h2 class="text-2xl font-bold mb-4 tracking-tight">New Event</h2>
+                <div>
+                </div>
+            </div>
+            <div class="flex gap-2 items-center justify-end p-4 bg-card rounded-b-lg shadow-neu-highlight">
+                <Button
+                    class="self-end"
+                    onclick={() => {
+                        newEventOverlayState.open = false;
+                    }}
+                >Add Event</Button>
+                <Button
+                    variant="secondary"
+                    class="self-end"
+                    onclick={() => {
+                        newEventOverlayState.open = false;
+                    }}
+                >Cancel</Button>
+            </div>
+        </div>
+    </div>
 </div>
 
-
-<style>
-    .overlay-container {
-        opacity: 0;
-        transform: translateY(10%) scale(0.25) scaleY(0.1);
-        transition: transform 0.35s cubic-bezier(.45, -.05, .15, 1.05),
-                    opacity 0.35s cubic-bezier(.45, -.05, .15, 1.05),
-                    filter 0.35s cubic-bezier(.45, -.05, .15, 1.05);
-        filter: blur(8px);
-    }
-
-    .overlay-container.open {
-        opacity: 1;
-        transform: translateY(0) scale(1) scaleY(1);
-        filter: blur(0);
-    }
-</style>
