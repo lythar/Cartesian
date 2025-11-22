c87c2886b57bd9e909e89e67ed40f437acb10e5f
Kyiro <Kyiro@protonmail.com>
2025-11-22 13:50:26 +0100

    feat: add aspire parameter for public mapbox token

--- Cartesian.AppHost/AppHost.cs
+++ Cartesian.AppHost/AppHost.cs
@@ -4,6 +4,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddDockerComposeEnvironment("compose");
 
+var mapboxPublicToken = builder.AddParameter("mapbox-public-token");
+
 var postgres = builder.AddPostgres("postgres")
     .WithImage("postgis/postgis:18-3.6-alpine")
     .WithHostPort(41021)
@@ -24,6 +26,7 @@ builder.AddNodeApp("frontend", "../Cartesian.Frontend", "dev")
     .WithRunScript("dev")
     .WithReference(services)
     .WithHttpEndpoint(env: "PORT", targetPort: 5173, isProxied: false)
+    .WithEnvironment("VITE_MAPBOX_ACCESS_TOKEN", mapboxPublicToken)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
